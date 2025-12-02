1a4eaeb25a18c0d735787cbe052668f9f9027bec
arqluk <alejandro.lukacher@gmail.com>
2025-12-01 21:08:04 -0300

    Final

--- src/models/RunnersFactory.js
+++ src/models/RunnersFactory.js
@@ -1,5 +1,4 @@
 import RunnersMemModel from "./DAO/runners.mem.model.js";
-// import RunnersFsModel from "./DAO/runners.fs.model.js";
 
 class RunnersFactory {
     static get(persistence) {
@@ -7,9 +6,6 @@ class RunnersFactory {
             case "MEM":
                 console.log("Persistiendo en la memoria del servidor.");
                 return new RunnersMemModel();
-            // case "FS":
-            //     console.log("Persistiendo en File System.");
-            //     return new RunnersFsModel();
             default:
                 console.log("Persistiendo en la memoria default.");
                 return new RunnersMemModel();
